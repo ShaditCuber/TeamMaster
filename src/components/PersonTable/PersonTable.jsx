43573b54b92024a1343d6ad88228aa2de4b4310b
Felipe Bastidas <felipebastidas2013@gmail.com>
2024-03-22 19:29:53 -0300

    Modificacion Para .env

--- src/components/PersonTable/PersonTable.jsx
+++ src/components/PersonTable/PersonTable.jsx
@@ -181,6 +181,7 @@ const PersonTable = ({ wcif, events }) => {
                     ))}
                 </tbody>
             </table>
+            
 
             <div className='flex flex-row'>
                 <div className="mt-4 w-1/3">
