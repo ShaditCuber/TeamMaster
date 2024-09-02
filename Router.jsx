54d060e8c58d56bf260b8e6fc5cb2fbcd27457ee
Felipe Bastidas <felipebastidas2013@gmail.com>
2024-09-02 00:13:57 -0400

    Rutas comentadas para ScoreCard

--- Router.jsx
+++ Router.jsx
@@ -5,13 +5,14 @@ import Error from "./src/pages/Error";
 import Layout from "./src/layout/Layout";
 import Competitions from "./src/pages/Competitions";
 import Competition from "./src/pages/Competition";
+import ScoreCard from "./src/pages/ScoreCard.jsx";
+import ScoreCardPDF from "./src/pages/ScoreCardPDF.jsx";
 
 
 
 
 const RouterApp = () => {
     const { user } = useUsuario();
-    console.log(user)
     const ProtectedRoute = ({ children }) => {
         return user ? <Layout>{children}</Layout> : <Home />;
     };
@@ -21,6 +22,11 @@ const RouterApp = () => {
             <Route path="/" element={user ? <Competition /> : <Home />} />
             <Route path="/competitions" element={<ProtectedRoute><Competitions /></ProtectedRoute>} />
             <Route path="/competitions/:id" element={<ProtectedRoute><Competition /></ProtectedRoute>} />
+            {/* <Route path="/scoreCard" element={<ProtectedRoute><Competitions isScorecard /></ProtectedRoute>} />
+            <Route path="/scoreCard/:id" element={<ProtectedRoute><ScoreCard /></ProtectedRoute>} /> */}
+            {/* <Route path="/PDF" element={<ProtectedRoute><ScoreCardPDF /></ProtectedRoute>} /> */}
+
+
             <Route path="*" element={<Layout><Error /></Layout>} />
         </Routes>
     )
